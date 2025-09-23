eb9e852d4a0822d76d68bc78fd289048e8cada49
Tharun-098 <tharun211388@gmail.com>
2025-09-23 12:20:31 +0530

    onclick

--- clients/src/components/Navbar.jsx
+++ clients/src/components/Navbar.jsx
@@ -4,8 +4,10 @@ import { Navigate } from 'react-router-dom';
 const Navbar = () => {
   return (
       <div className="flex items-center gap-2 p-4 md:pl-7 pl-4 bg-white shadow-md sticky top-0 z-999">
+        <div className='flex gap-2 items-center' onClick={()=>Navigate('/')}>
         <Calendar className="text-blue-700 font-bold md:w-10 md:h-10 w-7 h-7" />
-        <h1 className="font-bold md:text-2xl text-xl cursor-pointer" onClick={()=>Navigate('/')}>BookEasy</h1>
+        <h1 className="font-bold md:text-2xl text-xl cursor-pointer">BookEasy</h1>
+        </div>
       </div>
   )
 }
