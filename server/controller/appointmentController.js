0816141fd25c808361b26688e661203376163023
Tharun-098 <tharun211388@gmail.com>
2026-07-30 20:16:07 +0530

    kjk

--- server/controller/appointmentController.js
+++ server/controller/appointmentController.js
@@ -97,7 +97,7 @@ export const placeAppointment = async (req, res) => {
       ],
       mode: "payment",
       success_url: `https://booking-app-thhd.vercel.app/user/dashboard/records?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://booking-app-thhd.vercel.app/payment-cancel`,
+      cancel_url: `https://booking-app-thhd.vercel.app/user/dashboard/appointments`,
       metadata: {
         appointmentId: appointment._id.toString(),
       },
