64ed11edfd6a433d281764a9e81090a4e484a484
Tharun-098 <tharun211388@gmail.com>
2026-07-31 12:01:31 +0530

    solved

--- clients/src/components/ChatBotWidget.jsx
+++ clients/src/components/ChatBotWidget.jsx
@@ -92,7 +92,7 @@ const ChatBotWidget = () => {
               value={input}
               onChange={(e) => setInput(e.target.value)}
               placeholder="Type your symptoms..."
-              className="flex-1 border rounded-lg px-3 py-2 text-sm focus:outline-none"
+              className="sm:flex-1  border rounded-lg px-3 py-2 text-sm focus:outline-none"
             />
             <button
               onClick={handleSend}
